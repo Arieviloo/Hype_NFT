de9d5f6c29aeff03ee14a1fb41ea5ced69c7386b
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-08-22 15:40:55 -0300

    Adding TextField

--- Hype_NFT/Login/LoginView.swift
+++ Hype_NFT/Login/LoginView.swift
@@ -37,6 +37,40 @@ class LoginView: UIView {
         return $0
     }(UILabel())
     
+    lazy var emailTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.keyboardType = .emailAddress
+        $0.autocorrectionType = .no
+        $0.backgroundColor = UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1.0)
+        $0.clipsToBounds = true
+        $0.borderStyle = .roundedRect
+        $0.layer.cornerRadius = 12
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.white.cgColor
+        $0.textColor = .white
+        $0.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.4)])
+        
+        return $0
+    }(UITextField())
+    
+    lazy var passwordTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.keyboardType = .default
+        $0.isSecureTextEntry = true
+        $0.autocorrectionType = .no
+        $0.backgroundColor = UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1.0)
+        $0.clipsToBounds = true
+        $0.borderStyle = .roundedRect
+        $0.layer.cornerRadius = 12
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.white.cgColor
+        $0.textColor = .white
+        $0.attributedPlaceholder = NSAttributedString(string: "Senha", attributes: [
+            NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.4)
+        ])
+        return $0
+    }(UITextField())
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupAddSubview()
@@ -52,6 +86,8 @@ class LoginView: UIView {
         addSubview(logoImageView)
         addSubview(titleLabel)
         addSubview(subtitleLabel)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
     }
     
     private func configConstraints() {
@@ -70,7 +106,17 @@ class LoginView: UIView {
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             
             subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
-            subtitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            subtitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            emailTextField.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 20),
+            emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 8),
+            passwordTextField.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalTo: emailTextField.heightAnchor)
         ])
     }
 }
