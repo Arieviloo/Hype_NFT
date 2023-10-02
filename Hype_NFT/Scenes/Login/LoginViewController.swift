9b37303ef5280fc60970604ce6c0c3242a02eddf
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-10-02 11:51:40 -0300

    Validing text field

--- Hype_NFT/Scenes/Login/LoginViewController.swift
+++ Hype_NFT/Scenes/Login/LoginViewController.swift
@@ -15,7 +15,7 @@ class LoginViewController: UIViewController {
         loginView = LoginView()
         view = loginView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         dismissKeyBoard()
@@ -36,4 +36,37 @@ extension LoginViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    
+    func textFieldNotValid(_ textField: UITextField) {
+        textField.layer.borderWidth = 1.5
+        textField.layer.borderColor = UIColor.red.cgColor
+    }
+    
+    func textFieldIsValid(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text?.isEmpty ?? false {
+            textFieldNotValid(textField)
+        } else {
+            switch textField {
+            case self.loginView?.emailTextField:
+                if (loginView?.emailTextField.text ?? "").isValid(validType: .email) {
+                    textFieldIsValid(textField)
+                } else {
+                    textFieldNotValid(textField)
+                }
+            case self.loginView?.passwordTextField:
+                if (loginView?.passwordTextField.text ?? "").isValid(validType: .password) {
+                    textFieldIsValid(textField)
+                } else {
+                    textFieldNotValid(textField)
+                }
+            default:
+                break
+            }
+        }
+    }
 }
+
